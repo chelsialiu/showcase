861d5a4df3b2943f2684dd64e3a37fa580013dea
Chelsia Liu <chelsialiu@Chelsias-MacBook-Air.local>
2021-04-10 18:43:28 -0600

    Updating App.js to display date and Daily Quote from API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  ChakraProvider,
+  Grid,
+  Box,
+  Container,
+  Flex,
+  Button,
+  Text,
+  Divider,
+  Heading,
+} from "@chakra-ui/react";
+
+const config = {
+  apiUrl: "https://type.fit/api/quotes",
+};
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [quoteOfTheDay, setQuoteOfTheDay] = useState({});
+  const [today, setToday] = useState("");
+
+  const Quote = ({ text, author }) => {
+    return (
+      <Box w="100%" h="100%" bg="blue.500" textAlign="center">
+        {text} <br />
+        <br /> {author}
+      </Box>
+    );
+  };
+
+  const getQuotes = () => {
+    setQuotes([]);
+    setIsLoading(true);
+    fetch(config.apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then((data) => {
+        setQuotes(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const getDailyQuote = () => {
+    getQuotes();
+    getDate();
+    if (quotes.length > 0) {
+      let randomNum = Math.floor(Math.random() * quotes.length);
+      let dailyQuote = quotes[randomNum];
+      if (dailyQuote.author == "") {
+        dailyQuote.author = "Anonymous";
+      }
+      setQuoteOfTheDay(dailyQuote);
+      setIsLoading(false);
+    }
+  };
+
+  const getDate = () => {
+    let currentDate = new Date();
+    let cDay = currentDate.getDate();
+    let cMonth = currentDate.getMonth() + 1;
+    let cYear = currentDate.getFullYear();
+    let today = cMonth + "/" + cDay + "/" + cYear;
+    setToday(today);
+  };
+
+  if (isLoading) return <Text size="xl">Loading...</Text>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <Container maxWidth="100vm" backgroundColor="turquoise">
+        <Heading
+          as="h1"
+          size="4xl"
+          textAlign="center"
+          padding="4vh"
+          color="white"
+          backgroundColor="blue.700"
+          w="100%"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Quote of the Day
+        </Heading>
+        <Container minW="100vh" minH="50vh" centerContent padding="2vh">
+          <Heading as="h3" color="white" padding="3vh">
+            {today}
+          </Heading>
+          <Divider />
+          <Text
+            fontSize="4xl"
+            textAlign="center"
+            color="gray.600"
+            paddingTop="2vh"
+          >
+            {quoteOfTheDay.text}
+          </Text>
+          <Text
+            fontSize="2xl"
+            textAlign="center"
+            color="gray.600"
+            paddingTop="2vh"
+            paddingBottom="2vh"
+          >
+            {quoteOfTheDay.author}
+          </Text>
+          <Button onClick={() => getDailyQuote()} disabled={isLoading}>
+            See Another Quote
+          </Button>
+        </Container>
+        {/* <Flex backgroundColor="black">
+          <Button onClick={() => getQuotes()} disabled={isLoading}>
+            All Quotes
+          </Button>
+          <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+            {quotes.map((quote) => (
+              <Quote text={quote.text} author={quote.author}></Quote>
+            ))}
+          </Grid>
+        </Flex> */}
+      </Container>
+    </ChakraProvider>
   );
 }
 
